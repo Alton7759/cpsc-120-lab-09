742e835ec57bef791d2aeed1d277f662755e6883
Christian Bonilla <alton77@csu.fullerton.edu>
2022-11-11 08:19:31 -0800

    just got it done

--- part-2/spelling.cc
+++ part-2/spelling.cc
@@ -1,73 +1,70 @@
-// TODO: add a header
-
+// Christian Alton bonilla
+// CPSC 120-01
+// 2022-11-11
+// Alton77@csu.fullerton.edu
+// @alton7759
+//
+// Lab 09-02
+// Partners: @annavera38
+//
+// it spells checks
+//
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-
 // Read all the words from the given filename, and return them as a vector
 // of strings.
 // If the file cannot be opened, return an empty vector.
 std::vector<std::string> ReadWords(const std::string& filename) {
-  // TODO: declare an empty vector
-  // TODO: open a std::ifstream
-  // TODO: write a loop that reads each word from the ifstream, and
-  // uses push_back to add each word to the vector
-  // TODO: return the vector
-  return std::vector<std::string>(); // TODO: replace this return statement with one that actually works
+  std::vector<std::string> words;
+  std::ifstream input_file_stream{filename};
+  std::string line_buffer;
+  while (input_file_stream.good()) {
+    input_file_stream >> line_buffer;
+    words.push_back(line_buffer);
+  }
+  return words;
 }
 
-// Return true if word is present in dictionary, or false otherwise.
-// dictionary is intended to be a vector containing all words from a words.txt
-// file.
-// word is intended to be a string from a document file, that may or may not
-// be spelled properly.
 bool InDictionary(const std::vector<std::string>& dictionary,
                   const std::string& word) {
-  // TODO: write a linear search loop that determines whether or not
-  // word is present in dictionary.
-  // TODO: return true if word is present, or false otherwise
-  return false; // TODO: replace this return statement with one that actually works
+  bool indic = false;
+
+  for (const std::string& check : dictionary) {
+    if (word == check) {
+      indic = true;
+    }
+  }
+
+  return indic;
 }
 
-// Return a vector containing all of the misspelled words in document.
-// dictionary is intended to be a vector containing all words from a words.txt
-// file.
-// document is intended to be a vector of all of the words from a document
-// file.
 std::vector<std::string> MisspelledWords(
     const std::vector<std::string>& dictionary,
     const std::vector<std::string>& document) {
-  // TODO: declare an empty vector
-  // TODO: write a loop that checks each word string in document;
-  //   calls InDictionary to determine whether the word is in dictionary;
-  //   and adds the word to your vector if it not found
-  // TODO: return your vector
-  return std::vector<std::string>(); // TODO: replace this return statement with one that actually works}
+  std::vector<std::string> bad;
+  for (const auto& doc : document) {
+    if (!InDictionary(dictionary, doc)) {
+      bad.push_back(doc);
+    }
+  }
+
+  return bad;
+}
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
-
-  // TODO: validate that one argument was provided.
-  // If not, print
-  // error: you must give a document filename
-  // and return a non-zero exit code.
-
-  // TODO: store the first and only command line argument, which holds a
-  // document filename, in a variable
-
-  // TODO: call ReadWords to read the contents of words.txt, and store the
-  // return value in a variable for the dictionary
-
-  // TODO: call ReadWords to read the contents of the document filename,
-  // and store the return value in a variable for the document
-
-  // TODO: call MisspelledWords to find the list of all misspelled words, and
-  // store the return value in a variable for the output
-
-  // TODO: print
-  // spelling errors:
-  // and then use a loop to print out each of the misspelled words
-
+  if (arguments.size() != 2) {
+    std::cout << "error: you must give a document filename";
+    return 1;
+  }
+  arguments.erase(arguments.begin());
+  std::string filename{arguments.at(0)};
+  std::cout << "spelling errors:\n";
+  for (std::string& badwords :
+       MisspelledWords(ReadWords("words.txt"), ReadWords(filename))) {
+    std::cout << badwords << "\n";
+  }
   return 0;
 }
